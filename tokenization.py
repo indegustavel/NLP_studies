04b32fb95ee9317dbf972800d8cedebe4093c997
indegustavel <henriquegustavo@discente.ufg.br>
2026-02-15 11:25:24 -0300

    fix: correct function call for word tokenization

--- tokenization.py
+++ tokenization.py
@@ -2,5 +2,5 @@ import nltk
 
 text = "olá, eu sou o Gustavo. Estou estudando NLP e tokenização é uma parte importante disso."
 
-tokens = word_tokenizer(text)
+tokens = nltk.word_tokenize(text)
 print(tokens)
